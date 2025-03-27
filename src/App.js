ec3c478f04ea07c01c4eedfa3be15820727f1c42
safvan13058 <safvan13473@gmail.com>
2025-03-27 21:16:48 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./sections/nav/nav";
 import "./App.css";
 import Hero from "./sections/hero/hero";
@@ -10,10 +10,30 @@ import Banner from "./sections/banner/banner";
 import WhatIDo from "./sections/service/service";
 import ContactForm from "./sections/contact/contact";
 import Footer from "./sections/footer/footer";
+
 const App = () => {
+  useEffect(() => {
+    const handleOrientation = () => {
+      if (window.innerHeight > window.innerWidth) {
+        alert("Please rotate your device to landscape mode for the best experience.");
+      }
+    };
+    window.addEventListener("resize", handleOrientation);
+    handleOrientation();
+    return () => window.removeEventListener("resize", handleOrientation);
+  }, []);
+  useEffect(() => {
+    const setThemeColor = (color) => {
+      const metaThemeColor = document.querySelector("meta[name=theme-color]");
+      if (metaThemeColor) {
+        metaThemeColor.setAttribute("content", color);
+      }
+    };
+
+    setThemeColor("#F4F4F4"); // Set to your background color
+  }, []);
   return (
     <>
-    
       <Navbar />
       <div id="home" className="section"> <Hero/></div>
       <div id="about" className="section"><About/></div>
